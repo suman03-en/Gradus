1acec47a0d9b76fdcaacd33960a4981f79a8e9ac
suman phuyal <sfuyal59@gmail.com>
2026-01-22 23:06:45 +0545

    feat: add logout

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import RegisterUserAPIView, LoginView
+from .views import RegisterUserAPIView, LoginView, LogoutView
 
 urlpatterns = [
     path("register/",RegisterUserAPIView.as_view()),
     path("login/", LoginView.as_view()),
+    path("logout/", LogoutView.as_view()),
 ]
--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,10 +1,12 @@
 from rest_framework import generics
+from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework import status
 
 from django.contrib.auth import (
     login as django_login,
+    logout as django_logout,
 )
 from django.conf import settings
 from django.contrib.auth import get_user_model
@@ -19,6 +21,12 @@ class RegisterUserAPIView(generics.CreateAPIView):
 
 
 class LoginView(generics.GenericAPIView):
+    """
+    Calls Django Auth login method to register User ID
+    in Django session framework
+
+    Accept the following POST parameters: username, password
+    """
     permission_classes = (AllowAny, )
     serializer_class = LoginSerializer
 
@@ -39,7 +47,32 @@ class LoginView(generics.GenericAPIView):
             )
         self.serializer.is_valid(raise_exception=True)
         self.login()
-        return Response({"success":True, "msg":"login successfull."}, status=status.HTTP_200_OK)
+
+        response = Response({"details": "Successfully logged in."}, status=status.HTTP_200_OK)
+        
+        return response
 
         
+class LogoutView(APIView):
+    """
+    Calls Django logout method
 
+    Accepts/Returns nothing.
+    """
+    permission_classes = (IsAuthenticated, )
+
+        
+    def post(self, request, *args, **kwargs):
+        return self.logout(request)
+    
+    def logout(self, request):
+        """
+        For token authentication, delete the token .
+        and for session, call the django logout method.
+        """
+        if getattr(settings, 'REST_SESSION_LOGIN', True):
+            django_logout(request)
+
+        response = Response({"details": "Successfully logout."}, status=status.HTTP_200_OK)
+
+        return response
